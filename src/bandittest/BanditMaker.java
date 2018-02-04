4963049c9b9bcc6aad3f9a087d031f3408e4f590
Cup-of-cocoa <serohan_right2_yes1s573@yahoo.co.jp>
2018-02-04 22:49:41 +0900

    コメント追加

--- src/bandittest/BanditMaker.java
+++ src/bandittest/BanditMaker.java
@@ -8,6 +8,10 @@ import bandit.BanditProb;
 
 public class BanditMaker {
 
+	// ある確率で当たりを出すスロットマシンを作る。
+	// 「当たる確率」の配列を引数として渡すと、
+	// その確率で当たるスロットマシンの集合を返す。
+	// 配列のインデックスと、そのインデックスで与えられる確率で当たるスロットマシンが紐づけられる。
 	public Map<Integer, BanditProb> makeBanditBernoulli(double probs[]) {
 		Map<Integer, BanditProb> banditList = new HashMap<Integer, BanditProb>();
 		for (int i = 0; i < probs.length; i++) {
